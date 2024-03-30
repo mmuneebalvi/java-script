584077b078274dcef90953ca3f9b8557e1bc8a1f
mmuneebalvi <162284330+mmuneebalvi@users.noreply.github.com>
2024-03-30 12:00:34 +0500

    AD JS

---
+++ firstfile.js
@@ -0,0 +1,10 @@
+// import {a,user,user1,abcd} from "./secondfile.js";
+
+// console.log(a);
+// user("muneeb");
+
+// console.log(user1("muneeb"));
+// // console.log(abcd);
+// console.log(abcd[0]);
+// console.log(abcd[1]);
+// console.log(abcd[2]);
---
+++ secondfile.js
@@ -0,0 +1,11 @@
+// export let a= "MUNEEB";
+
+// export function user (name){
+//     console.log(`My name is ${name}`);
+// }
+
+// export function user1 (name){
+//     return`My name is ${name}`;
+// }
+
+// export let abcd= [101,"muneeb","city"];
